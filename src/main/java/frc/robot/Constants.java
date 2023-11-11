b643ec5af4ff4c4aa0e02e220260484c907c35df
trinity tribots <trinitytritons@gmail.com>
2023-11-11 07:36:17 -0800

    added controller id

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -3,6 +3,7 @@ package frc.robot;
 public class Constants {
     public static class OperatorConstants {
         public static final int kDriverControllerPort = 0;
+        public static final int kSubsystemControllerPort = 1;
       }
     
       public static class DriveConstants {
@@ -10,12 +11,12 @@ public class Constants {
         public static double kDeadzone = 0.4;
     
         public static final int kFrontLeftMotorId = 11;
-        public static final int kBackLeftMotorId = 14;
-        public static final int kFrontRightMotorId = 13;
         public static final int kBackRightMotorId = 12;
+        public static final int kFrontRightMotorId = 13;
+        public static final int kBackLeftMotorId = 14;
         public static final int kDumpMotorId = 15;
         public static final double kSlewValue = 1.0/ (1.0/2.0); // rate limit, ie bigger, less slew ie = 1/ time to max
-        public static double kStaticThrottleScalar = .3;
+        public static double kStaticThrottleScalar = 0.3;
     
       }
 }
