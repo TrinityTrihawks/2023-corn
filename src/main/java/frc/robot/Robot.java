dbcff5dad48417768a3be9d771b6311c775c19f2
WilliamBenish <willben496@gmail.com>
2023-11-18 14:45:23 -0600

    trying to use vision

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,6 +8,11 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.subsystems.Drivetrain;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+    import edu.wpi.first.networktables.NetworkTable;
+    import edu.wpi.first.networktables.NetworkTableEntry;
+    import edu.wpi.first.networktables.NetworkTableInstance;
+
 
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
@@ -45,7 +50,36 @@ public class Robot extends TimedRobot {
   }
 
   @Override
-  public void autonomousPeriodic() {}
+  public void autonomousPeriodic() {
+    NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+    NetworkTableEntry tx = table.getEntry("tx");
+    NetworkTableEntry ty = table.getEntry("ty");
+    NetworkTableEntry ta = table.getEntry("ta");
+
+    double x = tx.getDouble(0.0);
+    double y = ty.getDouble(0.0);
+    double area = ta.getDouble(0.0);
+    double drive = (10-area)/10;
+    double turn = x/100;
+    SmartDashboard.putNumber("turn for vision" , turn);
+    SmartDashboard.putNumber("drive for vision" , drive);
+    SmartDashboard.putNumber("LimelightX", x);
+    SmartDashboard.putNumber("LimelightY" , y);
+    SmartDashboard.putNumber("LimelightArea" , area);
+    /*if (area == 0){
+      Drivetrain.getInstance().drive(0, 0, 0); 
+    }else{
+      if (area < 10){
+      Drivetrain.getInstance().drive(drive, 0, 0);
+      }
+
+      if(x > 3) {
+      Drivetrain.getInstance().drive(0, 0, turn);
+      }else if(x < -3) {
+      Drivetrain.getInstance().drive(0, 0, turn);
+      }
+  }*/
+  }
 
   @Override
   public void autonomousExit() {}
